cddbad8ef9c08a8d95af5f8b118eac81fb80759e
kubachinIlya <mirretty@gmail.com>
2026-05-12 14:53:55 +0300

    #added data insert as from script

--- migrations/V003__insert_data.sql
+++ migrations/V003__insert_data.sql
@@ -26,3 +26,28 @@ DROP TABLE product;
 ALTER TABLE product_new RENAME TO product;
 ALTER TABLE orders_new RENAME TO orders;
 ALTER TABLE order_product_new RENAME TO order_product;
+
+
+-- тут генерация данных для новой схемы
+-- Вставляем продукты 
+INSERT INTO product (id, name, picture_url, price) VALUES
+(1, 'Сливочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/6.jpg', 320.00),
+(2, 'Особая', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg', 179.00),
+(3, 'Молочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/4.jpg', 225.00),
+(4, 'Нюренбергская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/3.jpg', 315.00),
+(5, 'Мюнхенская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg', 330.00),
+(6, 'Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg', 189.00);
+
+
+INSERT INTO orders (id, status, date_created)
+SELECT i,
+       (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)],
+       DATE(NOW() - (random() * (NOW()+'90 days' - NOW())))
+FROM generate_series(1, 10000000) s(i);
+
+ 
+INSERT INTO order_product (order_id, product_id, quantity)
+SELECT i,
+       1 + floor(random()*6)::int % 6,
+       floor(1+random()*50)::int
+FROM generate_series(1, 10000000) s(i);
